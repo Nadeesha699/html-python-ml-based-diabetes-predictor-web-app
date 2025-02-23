7e52ae30402b188f8ce74d3979b7c749cd6237d7
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-02-23 18:54:02 +0530

    saved

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np
+import pandas as pd
 app = Flask(__name__)
 CORS(app)
 with open('./model/trained_model.pkl','rb') as model_file:
@@ -24,7 +25,9 @@ def predict():
         data['DiabetesPedigreeFunction'],
         data['Age'],
     ]
-    input_scaled = scaler.transform([input_features])
+    feature_names = ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
+    input_df = pd.DataFrame([input_features],columns=feature_names)
+    input_scaled = scaler.transform(input_df)
     prediction = model.predict(input_scaled)
     if prediction[0] == 1:
         results = "Diabetic"
